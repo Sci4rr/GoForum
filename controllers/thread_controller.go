0fe651f1b2bd80150fac3e85f0d92665ebd0f607
Sci4rr <163331503+Sci4rr@users.noreply.github.com>
2024-12-05 14:10:36 +0100

    add new variables

--- controllers/thread_controller.go
+++ controllers/thread_controller.go
@@ -1,99 +1,99 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+    "encoding/json"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
 
-	"github.com/gorilla/mux"
+    "github.com/gorilla/mux"
 )
 
-type Thread struct {
-	ID          string `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
+type ForumThread struct {
+    ID          string `json:"id"`
+    Title       string `json:"title"`
+    Description string `json:"description"`
 }
 
-var threads []Thread
+var forumThreads []ForumThread
 
-func createThread(w http.ResponseWriter, r *http.Request) {
-	var newThread Thread
-	if err := json.NewDecoder(r.Body).Decode(&newThread); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	if newThread.Title == "" || newThread.Description == "" {
-		http.Error(w, "Missing title or description", http.StatusBadRequest)
-		return
-	}
-	threads = append(threads, newThread)
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(newThread)
+func createForumThread(w http.ResponseWriter, r *http.Request) {
+    var newForumThread ForumThread
+    if err := json.NewDecoder(r.Body).Decode(&newForumThread); err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+    if newForumThread.Title == "" || newForumThread.Description == "" {
+        http.Error(w, "Missing title or description", http.StatusBadRequest)
+        return
+    }
+    forumThreads = append(forumThreads, newForumThread)
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(newForumThread)
 }
 
-func getThreads(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(threads)
+func retrieveForumThreads(w http.ResponseWriter, r *http.Request) {
+    json.NewEncoder(w).Encode(forumThreads)
 }
 
-func getThreadByID(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for _, item := range threads {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-	http.Error(w, "Thread not found", http.StatusNotFound)
+func findForumThreadByID(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+    for _, thread := range forumThreads {
+        if thread.ID == params["id"] {
+            json.NewEncoder(w).Encode(thread)
+            return
+        }
+    }
+    http.Error(w, "Thread not found", http.StatusNotFound)
 }
 
-func updateThread(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for index, item := range threads {
-		if item.ID == params["id"] {
-			var updatedThread Thread
-			if err := json.NewDecoder(r.Body).Decode(&updatedThread); err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-				return
-			}
-			if updatedThread.Title != "" {
-				item.Title = updatedThread.Title
-			}
-			if updatedThread.Description != "" {
-				item.Description = updatedThread.Description
-			}
-			threads[index] = item
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-	http.Error(w, "Thread not found", http.StatusNotFound)
+func modifyForumThread(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+    for index, thread := range forumThreads {
+        if thread.ID == params["id"] {
+            var updatedThread ForumThread
+            if err := json.NewDecoder(r.Body).Decode(&updatedThread); err != nil {
+                http.Error(w, err.Error(), http.StatusBadRequest)
+                return
+            }
+            if updatedThread.Title != "" {
+                thread.Title = updatedThread.Title
+            }
+            if updatedThread.Description != "" {
+                thread.Description = updatedThread.Description
+            }
+            forumThreads[index] = thread
+            json.NewEncoder(w).Encode(thread)
+            return
+        }
+    }
+    http.Error(w, "Thread not found", http.StatusNotFound)
 }
 
-func deleteThread(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for index, item := range threads {
-		if item.ID == params["id"] {
-			threads = append(threads[:index], threads[index+1:]...)
-			break
-		}
-	}
-	w.WriteHeader(http.StatusNoContent)
+func removeForumThread(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+    for index, thread := range forumThreads {
+        if thread.ID == params["id"] {
+            forumThreads = append(forumThreads[:index], forumThreads[index+1:]...)
+            break
+        }
+    }
+    w.WriteHeader(http.StatusNoContent)
 }
 
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/threads", createThread).Methods("POST")
-	router.HandleFunc("/threads", getThreads).Methods("GET")
-	router.HandleFunc("/threads/{id}", getThreadByID).Methods("GET")
-	router.HandleFunc("/threads/{id}", updateThread).Methods("PUT")
-	router.HandleFunc("/threads/{id}", deleteThread).Methods("DELETE")
+    router := mux.NewRouter()
+    router.HandleFunc("/threads", createForumThread).Methods("POST")
+    router.HandleFunc("/threads", retrieveForumThreads).Methods("GET")
+    router.HandleFunc("/threads/{id}", findForumThreadByID).Methods("GET")
+    router.HandleFunc("/threads/{id}", modifyForumThread).Methods("PUT")
+    router.HandleFunc("/threads/{id}", removeForumThread).Methods("DELETE")
 
-	httpAddr := os.Getenv("HTTP_ADDR")
-	if httpAddr == "" {
-		httpAddr = ":8000"
-	}
+    httpAddr := os.Getenv("HTTP_ADDR")
+    if httpAddr == "" {
+        httpAddr = ":8000"
+    }
 
-	log.Fatal(http.ListenAndServe(httpAddr, router))
+    log.Fatal(http.ListenAndServe(httpAddr, router))
 }
